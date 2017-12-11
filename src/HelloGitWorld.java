e52fc5945e44b6b5d12176b5ad9756fc9db79127
Yamcha <dbals9812@naver.com>
2017-12-11 16:04:17 +0900

    update student number

--- src/HelloGitWorld.java
+++ src/HelloGitWorld.java
@@ -2,6 +2,6 @@
 public class HelloGitWorld {
 	public static void main(String args[]) {
         System.out.println("Hello, Git World!");
-        System.out.println("201700000");
+        System.out.println("201701992");
     }
 }
